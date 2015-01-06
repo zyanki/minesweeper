42db2f5c8bfbfffb147d271cfc9b3ee4892438ee
zyanki <yuki_muramatsu@cyberagent.co.jp>
2015-01-06 11:59:31 +0900

    遊び方(課題level4) 0.本プログラムをwindowsで行う場合はperlの実行環境を入れてください。linuxやmac等はデフォルトでperlが入っているので問題無いです。 ターミナルでプログラムを実行するとゲームが始まります。(例: perl minesweeper.pl)
    1.input a number of rows of panels (max 9)
    ゲーム画面の縦の大きさを決めます。1〜9の範囲で決めて下さい。(例:9)
    
    2.input a number of lines of panels (max 9)
    ゲーム画面の縦の大きさを決めます。1〜9の範囲で決めて下さい。(例:9)
    
    3.input a number of bombs of panels (max 81)
    爆弾の数を決めます。1〜81の範囲で決めて下さい。(例:1)
    
    4.input a num of row and char of line
    開くパネルの座標を指定します。先に英文字を入力し、次に数字を入力して下さい。(例: a0 )
    
    5.press o if you open the panel or x if you mark as a mine
    パネルを開くならoを押して下さい。開かないならxを押してマーキングをします。
    
    本プログラムはユーザに不親切なプログラムとなっており、遊び方を守らないと予期せぬ動きがおこります。
    遊び方を守って試して下さい。
    
    （また、本プログラムは再帰を使用せず、未リファクタリングのため汚いソースとなっています。
    時間があったら、直します。）

---
+++ minesweeper.pl
@@ -0,0 +1,166 @@
+use List::Util;
+@coord=();
+$bomb_num = ();
+@board=();
+@bomb = ();
+%open = ();
+@stack =();
+$hight =();
+$width =();
+
+print "input a number of rows of panels (max 9)";
+$hight = <STDIN>;
+chomp($hight);
+
+print "input a number of lines of panels (max 9)";
+$width = <STDIN>;
+chomp($width);
+
+print "input a number of bombs of panels (max 81)";
+$bomb_num = <STDIN>;
+chomp($bomb_num);
+
+print " ";
+for($i = 97; $i < (97+$hight); $i++) {
+	print chr($i) ;
+}
+print "\n";
+$bottom = 0;
+for($i = 0; $i < $hight; $i++) {
+	print "$i";
+	for($j = 0; $j < $width; $j++) {
+		print "?";
+		$board[$i][$j] = "?";
+		$coord[$bottom] = "$i:$j";
+		$bottom++;
+	}
+	print "\n";
+}
+
+@coord_shuffle = List::Util::shuffle @coord;
+
+for($i = 0; $i < $bomb_num; $i++) {
+	if($coord_shuffle[$i]=~/([0-9]):([0-9])/) {
+		$bomb[$1][$2] = "x";		
+	}
+}
+
+while(1){
+	print "input a num of row and char of line\n";
+	$coord = <STDIN>;
+	chomp $coord;
+	$y = chop($coord);
+	$char = chop($coord);
+	my $x = (ord $char ) - 97 ;
+	print "press o if you open the panel or x if you mark as a mine\n";
+	$open = <STDIN>;
+	chomp $open;
+	$bool = 0;
+	if($open eq "o") {
+		if( $bomb[$y][$x] eq "x" && $board[$y][$x] eq "?"){
+			print "Gameover\n";
+			exit(0);	
+		}elsif($board[$y][$x] eq "?") {
+			$panel_remain--;
+			$bool = 1;
+			$bomb_count = 0;
+			@stack =();
+			for($i = $y - 1; $i < $y + 2 ; $i++) {
+				for($j = $x - 1; $j < $x + 2 ; $j++) {
+					if($i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ){
+						next;
+					}
+					if($bomb[$i][$j] eq "x"){
+						$bomb_count++;
+					}
+				}
+			}
+		}else{
+
+		}
+
+		if($bomb_count !=0 && $bool == 1){
+			$board[$y][$x] = $bomb_count;
+		}elsif($bool == 1){
+			$board[$y][$x] = "0";
+			for($i = $y - 1; $i < $y + 2 ; $i++) {
+				for($j = $x - 1; $j < $x + 2 ; $j++) {
+					if($i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ){
+						next;
+					}
+					unless(exists($open{"$j$i"})) {
+						push(@stack,"$j$i");
+					}
+					
+				}
+			}
+			while($#stack != -1){
+				$coord = shift(@stack);
+				if(exists($open{"$coord"})){
+					next;
+				}
+				$y = chop($coord);
+				$x = chop($coord);
+				$bomb_count = 0;
+				@stack2 =();
+				for($i = $y - 1; $i < $y + 2; $i++) {
+					for($j = $x - 1; $j < $x + 2; $j++) {
+						if($i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ){
+							next;
+						}
+						
+						unless(exists($open{"$j$i"})){
+							$open{"$x$y"}++;
+							if($board[$i][$j] eq "?"){
+							push(@stack2,"$j$i");
+							}
+							if($bomb[$i][$j] eq "x"){
+								$bomb_count++;
+							}
+						}			
+					}
+				}
+				
+				$open{"$x$y"}++;
+				if($bomb_count != 0){
+					$board[$y][$x] = $bomb_count;
+				}else{
+					$board[$y][$x] = 0;
+					push @stack,@stack2;
+				}				
+			}
+
+		}else{
+
+		}
+	}elsif ($open eq "x") {
+		if($board[$y][$x] eq "?") {
+			$board[$y][$x] = "x";
+		}else{
+			$board[$y][$x] = "?";
+		}
+	}else{
+
+	}
+
+	print " ";
+	$panel_num=0;
+	for($i = 97; $i < (97+$hight); $i++) {
+		print chr($i) ;
+	}
+	print "\n";
+	for($i = 0; $i < $hight; $i++) {
+		print "$i";
+		for($j = 0; $j < $width; $j++) {
+			print "$board[$i][$j]";
+			if($board[$i][$j] eq "?" || $board[$i][$j] eq "x") {
+				$panel_num++;
+			}
+		}
+		print "\n";
+	} 
+	if($panel_num == $bomb_num){
+		print "clear!\n";
+		exit(0);
+	}
+}
