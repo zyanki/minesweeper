779ff8248b1196f8f0f081f0d1c7cd771a9cce13
zyanki <yuki_muramatsu@cyberagent.co.jp>
2015-03-26 10:48:27 +0900

    初期画面までを機能化して分割。

--- minesweeper.pl
+++ minesweeper.pl
@@ -1,120 +1,80 @@
 use List::Util;
-@coord=();
-$bomb_num = ();
-@board=();
-@bomb = ();
-%open = ();
-@stack =();
-$hight =();
-$width =();
 
-print "input a number of rows of panels (max 9)";
-$hight = <STDIN>;
-chomp($hight);
+&input_gameboard_size;
+&input_num_of_bomb;
+&show_initialized_screen;
+&romdom_bombset;
 
-print "input a number of lines of panels (max 9)";
-$width = <STDIN>;
-chomp($width);
-
-print "input a number of bombs of panels (max 81)";
-$bomb_num = <STDIN>;
-chomp($bomb_num);
-
-print " ";
-for($i = 97; $i < (97+$hight); $i++) {
-	print chr($i) ;
-}
-print "\n";
-$bottom = 0;
-for($i = 0; $i < $hight; $i++) {
-	print "$i";
-	for($j = 0; $j < $width; $j++) {
-		print "?";
-		$board[$i][$j] = "?";
-		$coord[$bottom] = "$i:$j";
-		$bottom++;
-	}
-	print "\n";
-}
-
-@coord_shuffle = List::Util::shuffle @coord;
-
-for($i = 0; $i < $bomb_num; $i++) {
-	if($coord_shuffle[$i]=~/([0-9]):([0-9])/) {
-		$bomb[$1][$2] = "x";		
-	}
-}
-
-while(1){
+while (1) {
 	print "input a num of row and char of line\n";
 	$coord = <STDIN>;
 	chomp $coord;
-	$y = chop($coord);
-	$char = chop($coord);
-	my $x = (ord $char ) - 97 ;
+	$y = chop( $coord );
+	$char = chop( $coord );
+	my $x = ( ord $char ) - 97 ;
 	print "press o if you open the panel or x if you mark as a mine\n";
 	$open = <STDIN>;
 	chomp $open;
 	$bool = 0;
-	if($open eq "o") {
-		if( $bomb[$y][$x] eq "x" && $board[$y][$x] eq "?"){
+	if ( $open eq "o" ) {
+		if ( $bomb[$y][$x] eq "x" && $board[$y][$x] eq "?" ) {
 			print "Gameover\n";
 			exit(0);	
-		}elsif($board[$y][$x] eq "?") {
+		} elsif ( $board[$y][$x] eq "?" ) {
 			$panel_remain--;
 			$bool = 1;
 			$bomb_count = 0;
-			@stack =();
-			for($i = $y - 1; $i < $y + 2 ; $i++) {
-				for($j = $x - 1; $j < $x + 2 ; $j++) {
-					if($i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ){
+			@stack = ();
+			for ( $i = $y - 1; $i < $y + 2 ; $i++ ) {
+				for ( $j = $x - 1; $j < $x + 2; $j++ ) {
+					if ( $i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ) {
 						next;
 					}
-					if($bomb[$i][$j] eq "x"){
+					if ( $bomb[$i][$j] eq "x" ) {
 						$bomb_count++;
 					}
 				}
 			}
-		}else{
+		} else {
 
 		}
 
-		if($bomb_count !=0 && $bool == 1){
+		if ( $bomb_count !=0 && $bool == 1) {
 			$board[$y][$x] = $bomb_count;
-		}elsif($bool == 1){
+		} elsif ( $bool == 1 ) {
 			$board[$y][$x] = "0";
-			for($i = $y - 1; $i < $y + 2 ; $i++) {
-				for($j = $x - 1; $j < $x + 2 ; $j++) {
-					if($i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ){
+			for ( $i = $y - 1; $i < $y + 2; $i++ ) {
+				for ( $j = $x - 1; $j < $x + 2; $j++ ) {
+					if ( $i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ) {
 						next;
 					}
-					unless(exists($open{"$j$i"})) {
+					unless ( exists( $open{"$j$i"} ) ) {
 						push(@stack,"$j$i");
 					}
 					
 				}
 			}
-			while($#stack != -1){
-				$coord = shift(@stack);
-				if(exists($open{"$coord"})){
+			while ( $#stack != -1) {
+				$coord = shift( @stack );
+				if ( exists( $open{"$coord"} ) ) {
 					next;
 				}
-				$y = chop($coord);
-				$x = chop($coord);
+				$y = chop( $coord );
+				$x = chop( $coord );
 				$bomb_count = 0;
 				@stack2 =();
-				for($i = $y - 1; $i < $y + 2; $i++) {
-					for($j = $x - 1; $j < $x + 2; $j++) {
-						if($i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ){
+				for ( $i = $y - 1; $i < $y + 2; $i++ ) {
+					for ( $j = $x - 1; $j < $x + 2; $j++ ) {
+						if ($i == $y && $j == $x || $i < 0 || $j < 0 || $i > $hight || $j > $width ) {
 							next;
 						}
 						
-						unless(exists($open{"$j$i"})){
+						unless( exists( $open{"$j$i"} ) ) {
 							$open{"$x$y"}++;
-							if($board[$i][$j] eq "?"){
-							push(@stack2,"$j$i");
+							if ( $board[$i][$j] eq "?") {
+								push( @stack2,"$j$i" );
 							}
-							if($bomb[$i][$j] eq "x"){
+							if ( $bomb[$i][$j] eq "x" ) {
 								$bomb_count++;
 							}
 						}			
@@ -122,45 +82,94 @@ while(1){
 				}
 				
 				$open{"$x$y"}++;
-				if($bomb_count != 0){
+				if ( $bomb_count != 0 ) {
 					$board[$y][$x] = $bomb_count;
-				}else{
+				} else {
 					$board[$y][$x] = 0;
 					push @stack,@stack2;
 				}				
 			}
 
-		}else{
+		} else {
 
 		}
-	}elsif ($open eq "x") {
-		if($board[$y][$x] eq "?") {
+	} elsif ($open eq "x") {
+		if ( $board[$y][$x] eq "?" ) {
 			$board[$y][$x] = "x";
-		}else{
+		} else {
 			$board[$y][$x] = "?";
 		}
-	}else{
+	} else {
 
 	}
 
 	print " ";
-	$panel_num=0;
-	for($i = 97; $i < (97+$hight); $i++) {
+	$panel_num = 0;
+	for ($i = 97; $i < (97+$hight); $i++ ) {
 		print chr($i) ;
 	}
 	print "\n";
-	for($i = 0; $i < $hight; $i++) {
+	for ( $i = 0; $i < $hight; $i++ ) {
 		print "$i";
-		for($j = 0; $j < $width; $j++) {
+		for ( $j = 0; $j < $width; $j++ ) {
 			print "$board[$i][$j]";
-			if($board[$i][$j] eq "?" || $board[$i][$j] eq "x") {
+			if ( $board[$i][$j] eq "?" || $board[$i][$j] eq "x" ) {
 				$panel_num++;
 			}
 		}
 		print "\n";
 	} 
-	if($panel_num == $bomb_num){
+	if ( $panel_num == $bomb_num ) {
 		print "clear!\n";
 		exit(0);
+	}
+}
+
+sub input_gameboard_size{
+	#ゲームボードの縦幅を決める
+	print "input a number of rows of panels (max 9)";
+	$hight = <STDIN>;
+	chomp( $hight );
+
+	#ゲームボードの横幅を決める
+	print "input a number of lines of panels (max 9)";
+	$width = <STDIN>;
+	chomp( $width );
+}
+
+sub input_num_of_bomb{
+	#地雷の数を決める
+	print "input a number of bombs of panels (max 81)";
+	$bomb_num = <STDIN>;
+	chomp( $bomb_num );
+}
+
+sub show_initialized_screen{
+	print " ";
+	#パネルの横を表示
+	for ( $i = 97; $i < (97+$hight); $i++ ) {
+		print chr($i) ;
+	}
+	print "\n";
+
+	$bottom = 0;
+	for ( $i = 0; $i < $hight; $i++ ) {
+		print "$i";
+		for ( $j = 0; $j < $width; $j++ ) {
+			print "?";
+			$board[$i][$j] = "?";
+			$coord[$bottom] = "$i:$j";
+			$bottom++;
+		}
+		print "\n";
+	}
+}
+
+sub romdom_bombset{
+	@coord_shuffle = List::Util::shuffle @coord;
+	for ( $i = 0; $i < $bomb_num; $i++ ) {
+		if ( $coord_shuffle[$i] =~ /([0-9]):([0-9])/ ) {
+			$bomb[$1][$2] = "x";		
+		}
 	}
 }
